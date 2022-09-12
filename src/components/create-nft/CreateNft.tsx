cd6bb350157c42dd996f3ecfd5e644584433b8a6
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-09-12 13:52:03 -0400

    3d model added and running

--- src/components/create-nft/CreateNft.tsx
+++ src/components/create-nft/CreateNft.tsx
@@ -10,20 +10,21 @@ import {
   Button,
   Box,
   BoxProps,
-} from "@chakra-ui/react";
-import {
   ListItem,
   UnorderedList,
   Text,
   Image,
   CircularProgress,
+  chakra,
 } from "@chakra-ui/react";
 // api
 import { GetProjectStatsQuery } from "hyperspace-client-js";
 // three.js
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import SceneInit from "../../../public/3dmodel/SceneInit";
+import SceneInit from "../../../public/SceneInit";
+
+import { Website3DDemo } from "../../../public/3dInit";
 
 type ProjectStatsType =
   GetProjectStatsQuery["getProjectStats"]["project_stats"];
@@ -40,49 +41,31 @@ const CreateNft: FunctionComponent<CreateNftProps> = (
   const [loading, setLoading] = useState(true);
   console.log("this is from create NFT page", projectStats);
 
-  //   useEffect(() => {
-  //     const test: any = new SceneInit("myThreeJsCanvas");
-  //     test.initialize();
-  //     test.animate();
-
-  //     let loadedModel: any;
-  //     const glftLoader = new GLTFLoader();
-  //     glftLoader.load("/3dmodel/scene.gltf", (gltfScene) => {
-  //       loadedModel = gltfScene;
-
-  //       gltfScene.scene.rotation.y = Math.PI / 8;
-  //       gltfScene.scene.position.y = 3;
-  //       gltfScene.scene.scale.set(10, 10, 10);
-  //       test.scene.add(gltfScene.scene);
-  //     });
-
-  //     const animate = () => {
-  //       if (loadedModel) {
-  //         loadedModel.scene.rotation.x += 0.01;
-  //         loadedModel.scene.rotation.y += 0.01;
-  //         loadedModel.scene.rotation.z += 0.01;
-  //       }
-  //       requestAnimationFrame(animate);
-  //     };
-  //     animate();
-  //   }, []);
-  //   if (loading) {
-  //     return <CircularProgress color="blue.400" isIndeterminate />;
-  //   } 37, 100, 235
+  let _APP: any = null;
+  useEffect(() => {
+    _APP = new Website3DDemo();
+    const handleScroll = (event: Event) => {
+      _APP.OnScroll(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <Box
-      layerStyle={"fullPageScrollEffectSection"}
-      backgroundColor={"hyperBlue"}
-      {...props}
-    >
+    <Box layerStyle={"fullPageScrollEffectSection"} {...props}>
       <Center>Create your NFT</Center>
       <Center>
         <Button variant="outline">Click me</Button>
       </Center>
-      {/* <Box>
-        <canvas id="myThreeJsCanvas" />
-      </Box> */}
+      <Center
+        id="model"
+        height={"400px"}
+        width={"600px"}
+        overflow={"hidden"}
+        margin={"0 auto"}
+      ></Center>
     </Box>
   );
 };
